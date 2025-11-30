97de041025eba3d97635ff7334042d5800b1eb62
tshmieldev <wojtekrok88@wp.pl>
2025-11-30 20:18:22 +0100

    v2

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { ChargingCalculator } from "../components/ChargingCalculator";
 import { Badge } from "@/components/ui/badge";
 import { Separator } from "@/components/ui/separator";
 import { ModeToggle } from "@/components/mode-toggle";
+import { cn } from "@/lib/utils";
 
 export default async function Home() {
     let energyData: DailyEnergyMix[] = [];
@@ -15,19 +16,23 @@ export default async function Home() {
 
     return (
         <main className="min-h-screen bg-background py-12 px-4 sm:px-6 lg:px-8 relative">
-            <div className="absolute top-4 right-4 md:top-8 md:right-8">
+            <div
+                className={cn(
+                    "absolute inset-0",
+                    "[background-size:20px_20px]",
+                    "[background-image:radial-gradient(#a4a4a4_1px,transparent_1px)]",
+                    "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
+                )}
+            />
+            <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_40%,black)] dark:bg-black"></div>
+            <div className="absolute z-1 top-4 right-4 md:top-8 md:right-8">
                 <ModeToggle />
             </div>
-            <div className="max-w-7xl mx-auto space-y-12">
+            <div className="max-w-7xl mx-auto space-y-12 relative">
                 <div className="text-center space-y-4">
-                    <h1 className="text-4xl font-bold text-foreground tracking-tight sm:text-5xl">
-                        UK Energy Mix Dashboard
+                    <h1 className="mb-16 text-3xl font-bold text-foreground tracking-tight sm:text-4xl">
+                        {"Wojciech's  UK Energy Mix Dashboard"}
                     </h1>
-                    <p className="text-lg text-muted-foreground max-w-2xl mx-auto">
-                        Monitor the current energy generation sources and
-                        optimize your EV charging schedule for the greenest
-                        energy usage.
-                    </p>
                 </div>
 
                 <section>
@@ -52,7 +57,7 @@ export default async function Home() {
                         <h2 className="text-2xl font-bold text-foreground mb-2">
                             EV Charging Optimizer
                         </h2>
-                        <p className="text-muted-foreground">
+                        <p className="text-foreground">
                             Find the best time to charge your vehicle based on
                             clean energy availability.
                         </p>
